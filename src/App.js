554cb2ca6b8cc98f587da1ffd5cc2511a918e0a3
Jordan Lesaca <jordanlesaca@MacBook-Pro.local>
2022-08-08 13:39:12 -0700

    added expense file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Expenses from './components/Expenses'
 
 const App = () => {
 
   return (
     <div>
       <h1>Page</h1>
+      <Expenses />
     </div>
   );
 };
---
+++ src/components/Expenses.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Expenses = (props) => {
+
+  return (
+    <div>
+      <h1>Expense Page</h1>
+    </div>
+  );
+};
+
+export default Expenses;
